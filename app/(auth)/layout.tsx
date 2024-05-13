b7244ecf3b7fe0195ad00c58c983ff66b1b9edb2
amitamrutiya2210 <amitamrutiya2210@gmail.com>
2024-05-13 11:40:33 +0530

    add authentication system
    Signed-off-by: amitamrutiya2210 <amitamrutiya2210@gmail.com>

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AuthLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div className="h-dvh flex items-center justify-center w-full">
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
