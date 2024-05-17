d5bfd4e3469f5bdc754df56fb5499cae6f34c73b
amitamrutiya2210 <amitamrutiya2210@gmail.com>
2024-05-17 10:11:18 +0530

    create responsive navbar
    Signed-off-by: amitamrutiya2210 <amitamrutiya2210@gmail.com>

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,9 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 export const OrgSidebar = () => {
   return (
     <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-400">
-      OrgSidebar
+
     </div>
   );
 };
