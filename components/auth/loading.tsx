42036897be10f11fb269101296605b106039f1dd
amitamrutiya2210 <amitamrutiya2210@gmail.com>
2024-05-17 11:13:31 +0530

    create organization switcher
    Signed-off-by: amitamrutiya2210 <amitamrutiya2210@gmail.com>

--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -4,10 +4,10 @@ export default function Loading() {
   return (
     <div className="h-full w-full flex flex-col gap-y-4 justify-center items-center">
       <Image
-        src="/logo.png"
+        src="/loading.png"
         width={120}
         height={120}
-        alt="Logo"
+        alt="Loading"
         className="animate-pulse duration-700"
       />
     </div>
