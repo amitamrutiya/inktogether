05f5cc0ef36db7bb79df2b223d48c55d29fbccff
amitamrutiya2210 <amitamrutiya2210@gmail.com>
2024-05-25 17:23:54 +0530

    create some mathod in utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,4 @@
+import { Camera, Color } from "@/app/types/canvas";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -11,3 +12,43 @@ export function cn(...inputs: ClassValue[]) {
 export const connectionIdToColor = (connectionId: number) => {
   return COLORS[connectionId % COLORS.length];
 };
+
+export function getSvgPathFromStroke(stroke: number[][]) {
+  if (!stroke.length) return "";
+
+  const d = stroke.reduce(
+    (acc, [x0, y0], i, arr) => {
+      const [x1, y1] = arr[(i + 1) % arr.length];
+      acc.push(x0, y0, (x0 + x1) / 2, (y0 + y1) / 2);
+      return acc;
+    },
+    ["M", ...stroke[0], "Q"]
+  );
+
+  d.push("Z");
+  return d.join(" ");
+}
+
+
+export function colorToCss(color: Color) {
+  return `#${color.r.toString(16).padStart(2, "0")}${color.g
+    .toString(16)
+    .padStart(2, "0")}${color.b.toString(16).padStart(2, "0")}`;
+}
+
+export const pointerEventToCanvasPoint = (
+  e: React.PointerEvent,
+  camera: Camera
+) => {
+  return {
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY) - camera.y,
+  };
+};
+
+
+export function getContrastingTextColor(color: Color) {
+  const luminance = 0.299 * color.r + 0.587 * color.g + 0.114 * color.b;
+
+  return luminance > 182 ? "black" : "white";
+}
